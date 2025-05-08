249c5a77e7776daca4664b45bb69f7206f8853f6
vivianli19 <341207876@gapps.yrdsb.ca>
2025-05-08 14:33:32 -0400

    Adjusting title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,8 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
-    
+    Label titleLabel = new Label("The Elephant", 60);
+
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,10 +18,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel,getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, getWidth()/2, 200);
+        prepare();
     }
-    
+
     /**
      * The main world act loop 
      */
@@ -33,5 +34,32 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,500,100);
+        elephant.setLocation(494,83);
+        elephant.setLocation(550,78);
+        elephant.setLocation(478,72);
+        Label label = new Label("Press <space> to Start", 20);
+        addObject(label,318,194);
+        label.setLocation(302,211);
+        Label label2 = new Label("Use <- and -> to move", 20);
+        addObject(label2,357,263);
+        label2.setLocation(303,247);
+        label2.setLocation(315,284);
+        label.setLocation(320,246);
+        label2.setLocation(323,285);
+        elephant.setLocation(317,136);
+        elephant.setLocation(302,114);
+        elephant.setLocation(298,140);
+        elephant.setLocation(306,122);
+        elephant.setLocation(305,102);
     }
 }
